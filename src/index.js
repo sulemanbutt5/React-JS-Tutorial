26e6245c5eb481b5645c5a4c97336c2401798efe
sulemanbutt5 <sulemanbutt5@gmail.com>
2020-08-25 22:01:46 +0500

    Newer Version

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-ReactDOM.render(<h1>SULEMAN</h1>, document.getElementById("root"));
+ReactDOM.render(<h1>Muhammad Suleman Butt</h1>, document.getElementById("root"));

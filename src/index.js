0781fc59b91559b5c144739db41bf6344d3dbb24
sulemanbutt5 <sulemanbutt5@gmail.com>
2020-08-25 22:11:19 +0500

    Changed Div

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-ReactDOM.render(<h1>Muhammad Suleman Butt</h1>, document.getElementById("root"));
+ReactDOM.render(<div><h1>This is the Heading</h1><p>This is the Paragraph</p></div>, document.getElementById("root"));

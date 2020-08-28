f31d8ac9e247c6d5bb9c19f4ddc06f4df7b214e1
sulemanbutt5 <sulemanbutt5@gmail.com>
2020-08-28 23:47:45 +0500

    ReactDom and JSX Practice

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 
-ReactDOM.render(<div><h1>This is the Heading</h1><p>This is the Paragraph</p></div>, document.getElementById("root"));
+ReactDOM.render(
+<ul>
+<li>1</li>
+<li>2</li>
+<li>3</li>
+</ul>,
+document.getElementById("root")
+)

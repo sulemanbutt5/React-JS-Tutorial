906a0011099fe6f93de420aa3ef4a9b3d0be112d
sulemanbutt5 <sulemanbutt5@gmail.com>
2020-08-29 00:20:14 +0500

    Functional Components Practice

--- src/index.js
+++ src/index.js
@@ -1,26 +1,26 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from "react"
+import ReactDom from "react-dom"
 
-function MyApp()
+function MyInfo()
 {
     return (
-    <div>
-        <ul>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-        </ul>
-        <ol>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-        </ol>
-    </div>
+        <div>
+            <h1>
+                MUHAMMAD SULEMAN BUTT
+            </h1>
+            <p>
+                My name is M. Suleman Butt. I am a boy.
+                I am 21 years old. I am becoming a Software Engineer.
+                I want to become a businessman as well. This is my webpage.
+                I want to visit these cities:
+            </p>
+            <ul>
+                <li>Paris</li>
+                <li>Islamabad</li>
+                <li>Istanbul</li>
+            </ul>
+        </div>
     )
 }
 
-ReactDOM.render(
-<MyApp />
-,
-document.getElementById("root")
-)
+ReactDom.render(<MyInfo/>,document.getElementById("root"))

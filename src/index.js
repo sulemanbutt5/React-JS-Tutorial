06a58044e1c54ad6379aadab8dba395d90ad6b88
sulemanbutt5 <sulemanbutt5@gmail.com>
2020-08-29 00:29:08 +0500

    Move Components into Separate Files

--- src/index.js
+++ src/index.js
@@ -1,26 +1,5 @@
 import React from "react"
 import ReactDom from "react-dom"
-
-function MyInfo()
-{
-    return (
-        <div>
-            <h1>
-                MUHAMMAD SULEMAN BUTT
-            </h1>
-            <p>
-                My name is M. Suleman Butt. I am a boy.
-                I am 21 years old. I am becoming a Software Engineer.
-                I want to become a businessman as well. This is my webpage.
-                I want to visit these cities:
-            </p>
-            <ul>
-                <li>Paris</li>
-                <li>Islamabad</li>
-                <li>Istanbul</li>
-            </ul>
-        </div>
-    )
-}
+import MyInfo from "./components/MyInfo" //or "./components/MyInfo.js"
 
 ReactDom.render(<MyInfo/>,document.getElementById("root"))

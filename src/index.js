ef4847575616790e9d2961f0c963aee412279f81
sulemanbutt5 <sulemanbutt5@gmail.com>
2020-08-29 00:00:49 +0500

    Functional Components

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+function MyApp()
+{
+    return (
+    <div>
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
+        <ol>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ol>
+    </div>
+    )
+}
+
 ReactDOM.render(
-<ul>
-<li>1</li>
-<li>2</li>
-<li>3</li>
-</ul>,
+<MyApp />
+,
 document.getElementById("root")
 )

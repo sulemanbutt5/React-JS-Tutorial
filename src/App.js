6e3651fe3529b8793d8781c0f91c1639d167525b
sulemanbutt5 <sulemanbutt5@gmail.com>
2020-09-04 09:32:31 +0500

    Mapping Components Practice

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from "react"
-import Jokes from "./Jokes"
+import Product from "./Product"
+import productsData from "./vschoolProducts"
 function App()
-{
+{    
+    const productComponents = productsData.map(item => <Product key={item.id} product={item}/>)
     return(
-        <div className="">
-            <Jokes que="Why did David Hasselhoff change his name to “The Hoff?”" ans="It’s less hassle."/>
-            <Jokes que="Why does Waldo wear stripes?" ans="Because he doesn’t want to be spotted."/>
-            <Jokes que="" ans="A man enters a pun contest in his local newspaper. He sends in ten puns, hoping at least one of them would win, but unfortunately, no pun in ten did."/>
-            <Jokes que="" ans="To be frank, I’d have to change my name."/>
-            <Jokes que="Why did the chicken go to prison?" ans="Crimes"/>
+        <div>
+            {productComponents}        
         </div> 
 
     )
---
+++ src/vschoolProducts.js
@@ -0,0 +1,39 @@
+const products=[
+    {
+        id:"1",
+        name: "Highlighter",
+        price: 25,
+        desc: "used to highlight the written data"
+    },
+    {
+        id:"2",
+        name: "Paper",
+        price: 3,
+        desc: "we can write on it"
+    },
+    {
+        id:"3",
+        name: "Ink",
+        price: 45,
+        desc: "used by pen to make writing visible"
+    },
+    {
+        id:"4",
+        name: "Pen",
+        price: 25,
+        desc: "writes which cannot be erased"
+    },
+    {
+        id:"5",
+        name: "Pencil",
+        price: 10,
+        desc: "writes which can be erased..."
+    },
+    {
+        id:"6",
+        name: "Eraser",
+        price: 5,
+        desc: "Erase things..."
+    }
+]
+export default products

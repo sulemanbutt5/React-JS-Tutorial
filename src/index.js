8c615d01a895617798de91a28f87e6983b8999f5
sulemanbutt5 <sulemanbutt5@gmail.com>
2020-09-04 05:44:44 +0500

    Inline Styles with Style Property

--- src/index.js
+++ src/index.js
@@ -8,27 +8,35 @@ function App()
     const date=new Date();
     var h = date.getHours()
     let timeofday
+    var styles ={
+        fontSize: "35px",
+        backgroundColor: "lightGreen"
+    }
     if (h<12) {
         timeofday="Morning"
+        styles.color= "Blue"
     }
     else if(h>=12 && h<18)
     {
         timeofday="Afternoon"
+        styles.color="Orange"
     }
     else
     {
         timeofday="Night"
+        styles.color="purple"
+
     }
 
     return(
         <div>
-            <h1>
+            <h1 style={styles}>
                 Hello {fn + " " + ln}!
             </h1>
-            <h2>
+            <h2 style={styles}>
                 Hello {`${fn} ${ln}`}!
             </h2>
-            <h3>
+            <h3 style={styles}>
                 Good {timeofday + " " + fn + " " + ln} 
             </h3>
         </div>

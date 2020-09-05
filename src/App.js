19aac79b260b8cb5cca16133759dc6eb50478ed4
sulemanbutt5 <sulemanbutt5@gmail.com>
2020-09-05 07:08:23 +0500

    Fetch Data from an API

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React from "react"
-import todosData from "./todosData"
-import TodoItem from "./TodoItem"
 import "./index.css"
 class App extends React.Component
 {    
@@ -8,30 +6,27 @@ class App extends React.Component
     {
         super()
         this.state={
-            todos: todosData
+            Loading: null,
+            character: {}
         }
-        this.hC=this.hC.bind(this)
     }
-    hC(id)
+    componentDidMount()
     {
-        this.setState(pr=>{
-            const upd=pr.todos.map(todo=>{
-                if (todo.id===id)
-                    todo.completed = !todo.completed
-                return todo
+        this.setState({Loading:true})
+        fetch("https://swapi.dev/api/people/4/").then(response=>response.json()).then(data=>{
+            this.setState({
+                Loading: false,
+                character: data
             })
-            return {
-                todos:upd
-            }
         })
-
     }
     render()
     {
-        const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item ={item} hC={this.hC}/>)
+        let text=this.state.Loading?"Loading...":this.state.character.name
         return(
-            <div className="todo-list">
-                {todoItems}
+
+            <div>
+                <h1>{text}</h1>
             </div> 
 
         )

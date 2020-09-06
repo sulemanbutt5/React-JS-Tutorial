4f927470b5c6fcc4edb24de4cf333e471fcafa07
sulemanbutt5 <sulemanbutt5@gmail.com>
2020-09-06 06:07:55 +0500

    Meme Generator Final

--- src/Header.js
+++ src/Header.js
@@ -9,6 +9,7 @@ import React from "react"
                  src="http://pngall.com/wp-content/uploads/2016/05/Trollface.png" 
                  alt="Problem?"
                  />
+                 <p>Meme Generator</p>
              </header>
          </div>
      )
